4a202403ed5398327ac36703e46906ba3c27b4e2
heysivani <heysivani@gmail.com>
2022-03-22 15:50:53 +0900

    :sparkles: feat(image-upload): user can upload images up to 100mb

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use("/posts", postRoutes);
 
