cbd7fd6a7becafa1d20941ce9c061efef5aaf3fd
heysivani <heysivani@gmail.com>
2022-03-22 19:06:27 +0900

    :rocket: deploy: point base url to deployed backend staging app

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const fetchPosts = () => axios.get("/posts");
-export const addPost = (post) => axios.post("/posts", post);
-export const updatePost = (id, post) => axios.patch(`posts/${id}`, post);
-export const likePost = (id) => axios.patch(`/posts/${id}/likes`);
-export const deletePost = (id) => axios.delete(`/posts/${id}`);
+const url = "https://gushy-api-staging.herokuapp.com";
+
+export const fetchPosts = () => axios.get(`${url}/posts`);
+export const addPost = (post) => axios.post(`${url}/posts`, post);
+export const updatePost = (id, post) => axios.patch(`${url}/posts/${id}`, post);
+export const likePost = (id) => axios.patch(`${url}/posts/${id}/likes`);
+export const deletePost = (id) => axios.delete(`${url}/posts/${id}`);
