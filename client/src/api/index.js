4b2995767e163317a0d014d0da2d80afbfdcdb5f
heysivani <heysivani@gmail.com>
2022-03-24 12:21:29 +0900

    :bug: fix: update variable name to match env variable and add env file for production

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = process.env.NETLIFY ? API : "";
 
-export const fetchPosts = () => axios.get(`${URL}/posts`);
-export const addPost = (post) => axios.post(`${URL}/posts`, post);
-export const updatePost = (id, post) => axios.patch(`${URL}/posts/${id}`, post);
-export const likePost = (id) => axios.patch(`${URL}/posts/${id}/likes`);
-export const deletePost = (id) => axios.delete(`${URL}/posts/${id}`);
+export const fetchPosts = () => axios.get(`${API}/posts`);
+export const addPost = (post) => axios.post(`${API}/posts`, post);
+export const updatePost = (id, post) => axios.patch(`${API}/posts/${id}`, post);
+export const likePost = (id) => axios.patch(`${API}/posts/${id}/likes`);
+export const deletePost = (id) => axios.delete(`${API}/posts/${id}`);
