1835e86b3a08f3e891db4725a1dd262e9d1b32d9
heysivani <heysivani@gmail.com>
2022-03-24 11:59:00 +0900

    :recycle: refactor: api url points to heroku url if running on netlify (and defaults to dev proxy otherwise)

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const url = "https://gushy-api-staging.herokuapp.com";
+const API = process.env.NETLIFY ? API : "";
 
-export const fetchPosts = () => axios.get(`${url}/posts`);
-export const addPost = (post) => axios.post(`${url}/posts`, post);
-export const updatePost = (id, post) => axios.patch(`${url}/posts/${id}`, post);
-export const likePost = (id) => axios.patch(`${url}/posts/${id}/likes`);
-export const deletePost = (id) => axios.delete(`${url}/posts/${id}`);
+export const fetchPosts = () => axios.get(`${URL}/posts`);
+export const addPost = (post) => axios.post(`${URL}/posts`, post);
+export const updatePost = (id, post) => axios.patch(`${URL}/posts/${id}`, post);
+export const likePost = (id) => axios.patch(`${URL}/posts/${id}/likes`);
+export const deletePost = (id) => axios.delete(`${URL}/posts/${id}`);
